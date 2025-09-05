fca196d3b0850be2f681326cf60c377063fefb5e
Billy Jaffray <billyjaf@outlook.com>
2025-09-05 12:00:13 +1000

    fix: moved out of subdirectory, deleted backend

--- Chess-REST-API-Rust/src/health/mod.rs
+++
@@ -1,9 +0,0 @@
-use axum::{
-    Json,
-};
-
-pub async fn health_check() -> Json<String> {
-    println!("Received a health check!");
-    let health_message = String::from("Up and running!");
-    Json::from(health_message)
-}
